2f9768c69b2e3c9a03a0d80bdcda89c62b133b82
Juwencheng <juwenz@icloud.com>
2018-09-24 09:53:05 +0800

    添加说明注释

--- JWCTableViewController/Classes/JWCTableViewController.h
+++ JWCTableViewController/Classes/JWCTableViewController.h
@@ -26,21 +26,54 @@
 
 
 @interface JWCTableViewController : UIViewController
-
+/**
+ * 刷新列表数据
+ * @param data NSArray<JWCTableViewSectionData *>
+ */
 - (void)reloadData:(NSArray<JWCTableViewSectionData *> *)data;
 
+/**
+ * 添加数据 data 到某个 section，并刷新
+ * @param data NSArray<JWCTableViewCellData *>
+ * @param section section number
+ */
 - (void)appendData:(NSArray<JWCTableViewCellData *> *)data toSection:(NSInteger)section;
 
+/**
+ * 追加更多数据并刷新列表
+ * @param moreData NSArray<JWCTableViewSectionData *>
+ */
 - (void)loadMoreData:(NSArray<JWCTableViewSectionData *> *)moreData;
 
+/**
+ * 删除某个 section 并刷新列表
+ * @param section NSInteger
+ */
 - (void)removeSection:(NSInteger)section;
 
+/**
+ * 删除某个 section 的中的 data 数据，并刷新列表
+ * @param data NSArray <JWCTableViewCellData *>
+ * @param section NSInteger
+ */
 - (void)removeData:(NSArray <JWCTableViewCellData *> *)data fromSection:(NSInteger)section;
 
+/**
+ * 应用配置
+ * @param configure JWCTableViewConfigure
+ */
 - (void)applyConfigure:(JWCTableViewConfigure *)configure;
 
+/**
+ * 配置tableView，子类继承
+ */
 - (void)configureTableView;
 
+/**
+ * 快捷方法生成 JWCTableViewController
+ * @param configure JWCTableViewConfigure
+ * @return JWCTableViewController
+ */
 + (instancetype)tableViewControllerWithConfigure:(JWCTableViewConfigure *)configure;
 
 @property(nonatomic, strong, readonly) UITableView *tableView;
