e84cde5de9bee58d87d5e8b860bdf40ad85f578d
Juwencheng <juwenz@icloud.com>
2018-09-25 19:53:26 +0800

    更新 demo

--- Example/JWCSettingViewController.m
+++ Example/JWCSettingViewController.m
@@ -66,7 +66,7 @@
 }
 
 - (void)registerReuseCells {
-    [self registReuserCellClass:[JWCSettingCell class] withCellDataClass:[JWCSettingCellItem class]];
+    [self.tableView registReuserCellClass:[JWCSettingCell class] withCellDataClass:[JWCSettingCellItem class]];
 }
 
 @end
